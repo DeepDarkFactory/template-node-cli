f37868ca920aa94a48cded6c67bfd68941117a20
Kevin Lanni <therealklanni@gmail.com>
2017-06-15 21:19:26 -0700

    Fix .eslintrc

--- sao.test.js
+++ sao.test.js
@@ -1,22 +1,22 @@
-const { resolve } = require('path');
-const test = require('ava');
-const sao = require('sao');
-
+const test = require(`ava`)
+const { resolve } = require(`path`)
+const sao = require(`sao`)
 const template = {
-  fromPath: resolve('.'),
-};
+  fromPath: resolve(`.`)
+}
 
-test('Copyright has correct info', (t) => {
-  return sao.mockPrompt(template, {
-    username: 'someuser',
-    name: 'Some User',
-  })
-  .then((res) => {
-    const thisYear = new Date().getFullYear();
-    const content = res.files['LICENSE'].contents.toString();
-    const headerText = content.match(/Copyright(.*)/)[1];
-    t.not(headerText.indexOf(thisYear), -1);
-    t.not(headerText.indexOf('github.com/someuser'), -1);
-    t.not(headerText.indexOf('Some User'), -1);
-  });
-});
+test(`Copyright has correct info`, t =>
+  sao
+    .mockPrompt(template, {
+      username: `someuser`,
+      name: `Some User`
+    })
+    .then(res => {
+      const thisYear = new Date().getFullYear()
+      const content = res.files.LICENSE.contents.toString()
+      const headerText = content.match(/Copyright \(c\) (.*)$/im)[1]
+
+      t.not(headerText.indexOf(thisYear), -1)
+      t.not(headerText.indexOf(`github.com/someuser`), -1)
+      t.not(headerText.indexOf(`Some User`), -1)
+    }))
